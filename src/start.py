f38252882663cc8641237a762af02925ea7ab591
Dornavan <130479724+dorndickence@users.noreply.github.com>
2024-02-26 03:34:47 +0300

    Update start.py

--- src/start.py
+++ src/start.py
@@ -11,7 +11,7 @@
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as chromeservice
-from webdriver_manager.chrome import chromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep, strftime
